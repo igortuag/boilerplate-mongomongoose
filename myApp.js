15ce196c63177d0b702ebab8225d99c2a9b4a85b
Tuag <igortuag@gmail.com>
2021-07-08 07:34:44 -0300

    add configs

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
+/** # MONGOOSE SETUP #
+/*  ================== */
+
+/** 1) Install & Set up mongoose */
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
 
 let Person;
 
